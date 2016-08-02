919d50d83e2d06cf8c37914372d7fad4de583af2
twant <taylor.want@gmail.com>
2016-08-02 14:46:54 +0000

    pig latinize and klossify methods

--- models/model.rb
+++ models/model.rb
@@ -4,6 +4,7 @@
 def years_alive(birthday)
   #days in the months array
   days_in_months=[31,28,31,30,31,30,31,31,30,31,30,31]
+  
   #current date 
   the_time=Time.now.to_s.split(" ")
   date=the_time[0].split("-")
@@ -60,3 +61,33 @@ def years_alive(birthday)
   #total of everything
  days_age+years_age+leap_years
 end
+
+
+
+def klossify(phrase)
+  phrase.gsub("c","k").gsub("C","K")
+end
+
+
+ def piglatinize(word)
+    non_pig_latin_words = ["i", "me", "to", "too", "a", "an", "in", "and", "on"]
+    vowels = ["a", "e", "i", "o", "u"]
+ 
+      if non_pig_latin_words.include?(word)
+        word
+       elsif vowels.include? word[0]
+        word << "ay"
+       else
+         consonants = ""
+       while !vowels.include?(word[0])
+         consonants << word[0]
+         word = word.split("")[1..-1].join
+       end
+       word + consonants + 'ay'
+   end
+ end
+ 
+
+ def to_pig_latin(string)
+   string.split.collect{|word| piglatinize(word)}.join(" ")
+ end
